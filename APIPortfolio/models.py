99c5acbdaa028a7c361b5d75e18ba23c31a9abe9
Samuel Jurado Quintana <samueljurado12@gmail.com>
2019-02-05 18:15:26 +0100

    Created models.py without doc
    Co-Authored-By: Aleks <alkesst@users.noreply.github.com>

---
+++ APIPortfolio/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=25, blank=False, unique=True)
+
+    def to_dict(self):
+        return {'id': self.id, 'tag': self.tag}
+
+    def __str__(self):
+        return '{}'.format(self.tag)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+    image_url = models.ImageField(upload_to='', default='/media/')
+    date_added = models.DateField(auto_now_add=True)
+    project_tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectContrib(Project):
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    GITHUB = 'GH'
+    ITCH = 'IO'
+    WEB = 'WB'
+    LINK_TYPE_CHOICES = (
+        (ITCH, 'ITCH.IO'),
+        (GITHUB, 'GITHUB'),
+        (WEB, 'WEBPAGE')
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='links')
+    link = models.CharField(max_length=150)
+    link_type = models.CharField(max_length=2, choices=LINK_TYPE_CHOICES, default=GITHUB)
+
+    def __str__(self):
+        return self.project.name + ': ' + self.link_type
+
+
+class Screenshot(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='screenshots_reference')
+    image = models.ImageField(upload_to='project_images/screenshots', default='/media/')
+
+    def __str__(self):
+        return self.project.name + '_' + self.id.__str__()
